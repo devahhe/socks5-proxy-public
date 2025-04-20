733b62aa801cd72837a5002eb6b7ad32eb649d94
devahhe <dryzdevil@gmail.com>
2025-04-20 08:32:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const http = require('http');
+const http = require("http");
+const { spawn } = require("child_process");
+
 const port = process.env.PORT || 10080;
 
-http.createServer((req, res) => {
+const server = http.createServer((_, res) => {
   res.writeHead(200);
-  res.end("SOCKS5 Proxy is running.\n");
-}).listen(port, () => {
+  res.end("SOCKS5 proxy is running\n");
+});
+
+server.listen(port, () => {
   console.log(`HTTP server listening on port ${port}`);
+
+  const sockd = spawn("sockd", ["-f", "/etc/sockd.conf"]);
+  sockd.stdout.on("data", (data) => console.log(data.toString()));
+  sockd.stderr.on("data", (data) => console.error(data.toString()));
 });
